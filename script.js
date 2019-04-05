d6dfc52aac4d8464459009c363e1f12b36291af8
Louis fabrizio <louisfabrizio.student@careerdevs.com>
2019-04-05 00:21:59 +0000

    api

--- script.js
+++ script.js
@@ -1,63 +1,18 @@
-var googleSpreadsheet = require("google-spreadsheet");
-var doc = new GoogleSpreadsheet('1DgHYi6ETHtqU7nkO25PSr_eGydu37LU6sSSR1et1pY8');
+<script data-require="jquery@*" data-semver="3.2.1" src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
 
+<script>
+  var url = 'https://spreadsheets.google.com/feeds/list/1j2K7lx7h_yQxCW9iwKBHVp2pxrB5nYeQv1onP9QVbZg/1/public/values?alt=json';
 
+        $.getJSON(url, function(data) {
+            var headInfo = data.feed.title.$t;
+            $('#heading').html('<h1>' + headInfo + '</h1>');
+            var entryData = data.feed.entry;
+            jQuery.each(entryData, function() {
+                $('.tBody').append('<div class="tRow"><div class="tCell">' + this.gsx$name.$t + '</div><div class="tCell">' + this.gsx$department.$t + '</div><div class="tCell">' + this.gsx$salary.$t + '</div></div>');
 
+            })
 
-
-function getSpreadsheetData(sheetName) {
-    // This function gives you an array of objects modeling a worksheet's tabular data, where the first items — column headers — become the property names.
-    var arrayOfArrays = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName || 'Sheet1').getDataRange().getValues();
-    var headers = arrayOfArrays.shift();
-    return arrayOfArrays.map(function (row) {
-      return row.reduce(function (memo, value, index) {
-        if (value) {
-          memo[headers[index]] = value;
-        }
-        return memo;
-      }, {});
-    });
-  }
-
-//////////////////////////////
-
-function successFunc(data) {
-    console.log(data);
-  }
-  
-
-
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-
-  ss.Sheetsu.read(
-    "https://sheetsu.com/apis/v1.0lw/020b2c0f/",
-    {},
-    successFunc
-  );
-
-
-
-
-
-///////////////////
-
-
-var Employee = {
-    firstname: "NAME1",
-    lastname: "LASTNAME1",
-    idnumber: "employeeID",
-
-
-
-
-}
-
-
-function renderEmployeeInfo() {
-    console.log("test");
-    for (var i = 0; i < Employee.length; i++)
-    document.getElementById("here").appendChild(h6).innerHTML = Employee[i].firstname;
-}
-
+        });
+  </script> 
 
 
